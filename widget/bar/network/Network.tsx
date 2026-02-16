d81a145b43612ef86a3d29cdb812317c545a8117
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-16 18:15:45 +0100

    network: fix width to 24 chars

--- widget/bar/network/Network.tsx
+++ widget/bar/network/Network.tsx
@@ -33,7 +33,7 @@ export default function Network() {
 	return (
 		<box class="network-container">
 			<label visible={false} label={_tick} />
-			<label class="sysinfo-value" label={val} widthChars={20} />
+			<label class="sysinfo-value" label={val} widthChars={24} maxWidthChars={24} />
 		</box>
 	)
 }
