c43fcf24c552a4c00ecc321c74f6087cc0495a92
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-04 11:30:03 +0200

    import notification widget

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
+import Notifications, { NOTIF_MONITOR } from "./widget/notifications/Notifications"
 
 app.start({
   css: style,
   main() {
     app.get_monitors().map(Bar)
+    const monitors = app.get_monitors()
+    Notifications(monitors[Math.min(NOTIF_MONITOR, monitors.length - 1)])
   },
 })
