d3dfa8a3a0a461187d3802a10f3d815932f82ab8
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-07-15 08:31:05 +0200

    Revert "declare textdecoder global for gjs"
    This reverts commit 60a3d7fe7606f41ee109d5e0e3588e7337e7ba2e.

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,5 @@
 declare const SRC: string
 
-// GJS provides the WHATWG Encoding API as globals; not in the ES lib
-declare class TextDecoder {
-  constructor(label?: string)
-  decode(input?: ArrayBufferView | ArrayBuffer): string
-}
-
 declare module "inline:*" {
   const content: string
   export default content
